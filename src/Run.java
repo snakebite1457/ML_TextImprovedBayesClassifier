13576e915c4f9ad9fcdb01ea4a4bb339543f4f34
Sebastian Brodehl <sbrodehl@students.uni-mainz.de>
2014-07-19 13:46:24 +0200

    Now with fancy %

--- src/Run.java
+++ src/Run.java
@@ -19,13 +19,19 @@ public class Run {
         System.out.println("### Calculating improved counts for documents: ###");
         int allTrainingSets = trainDocuments.size();
         int current = 0;
+        int percentage = 0;
+        System.out.print("%");
         for (Document document : trainDocuments) {
             document.calculateImprovedCounts(trainDocuments);
-            System.out.println(++current + "/" + allTrainingSets);
+            //System.out.println(++current + "/" + allTrainingSets);
+            if (++current % (allTrainingSets/100) == 0) {
+                System.out.print(" " + ++percentage);
+            }
         }
+        System.out.println();
         System.out.println("### DONE ###");
         long endTime = System.currentTimeMillis();
-        System.out.println(endTime - startTime + "ms");
+        System.out.println((endTime - startTime)/1000 + "s");
 
         BayesTextClassifier bayesTextClassifier = new BayesTextClassifier(trainDocuments);
 
@@ -41,7 +47,7 @@ public class Run {
         }
         System.out.println("### DONE ###");
         endTime = System.currentTimeMillis();
-        System.out.println(endTime - startTime + "ms");
+        System.out.println((endTime - startTime)/1000 + "s");
         System.out.println(countCorrect / testDocuments.size() + " correct labeled documents.");
     }
 
@@ -50,13 +56,15 @@ public class Run {
             BufferedReader br = new BufferedReader(new FileReader(filePath));
             String line = br.readLine();
             int allDocuments = 0;
+            int current = 0;
+            int percentage = 0;
             while (line != null) {
                 allDocuments++;
                 line = br.readLine();
             }
             br = new BufferedReader(new FileReader(filePath));
             line = br.readLine();
-            int currentDoc = 0;
+            System.out.print("%");
             while (line != null) {
                 Document document = new Document(line.substring(3), line.substring(0, 1));
                 if (Math.random() < 0.75) {
@@ -64,9 +72,12 @@ public class Run {
                 } else {
                     testDocuments.add(document);
                 }
-                System.out.println(++currentDoc + "/" + allDocuments);
+                if (++current % (allDocuments/100) == 0) {
+                    System.out.print(" " + ++percentage);
+                }
                 line = br.readLine();
             }
+            System.out.println();
         } catch (IOException e) {
             e.printStackTrace();
         }
